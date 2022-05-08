aa88577e224b63363378dd3ab63e4b9d21c097ff
Zhanbolat Orakbayev <fous363@gmail.com>
2022-05-08 16:57:55 +0600

    Fix wrong behaviour in map

--- task03-map.go
+++ task03-map.go
@@ -6,14 +6,14 @@ func sortMapValues(input map[int]string) (result []string) {
 	if len(input) == 0 {
 		return []string{}
 	}
-	keys := make([]int, 0, len(input))
+	keys := make([]int, len(input))
 	for k := range input {
 		keys = append(keys, k)
 	}
 
 	sort.Ints(keys)
 
-	result = make([]string, 0, len(input))
+	result = make([]string, len(input))
 	for i := 0; i < len(input); i++ {
 		result[i] = input[keys[i]]
 	}
