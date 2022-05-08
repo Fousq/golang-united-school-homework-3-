333cc538041d50a63f141047c4bc9ec6e14bc4be
Zhanbolat Orakbayev <fous363@gmail.com>
2022-05-08 16:42:51 +0600

    Fix the slice

--- task02-slice.go
+++ task02-slice.go
@@ -4,14 +4,10 @@ func reverse(input []int64) (result []int64) {
 	if len(input) == 0 {
 		return input
 	}
-	reversedInput := make([]int64, len(input))
 
-	copy(reversedInput, input)
-
-	for i := 0; i < len(reversedInput)/2; i++ {
-		j := len(reversedInput) - i
-		reversedInput[i], reversedInput[j] = reversedInput[j], reversedInput[i]
+	for i := len(input); i > -1; i-- {
+		result = append(result, input[i])
 	}
 
-	return reversedInput
+	return result
 }
